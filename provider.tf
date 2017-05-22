cff0efd27d95eb1e80fb0009965b6ad5332e1b58
Sean Chittenden <sean@chittenden.org>
2017-05-21 18:15:00 -0700

    Update the comments in `provider.tf` to be more self-explanatory.

--- provider.tf
+++ provider.tf
@@ -1,11 +1,20 @@
 # Configure with parameters from `triton env` or `triton env $(PROFILE_NAME)`
 provider "triton" {
-  account      = "my-triton-account-name"   # Value from TRITON_PROFILE
-  key_material = "${file("~/.ssh/id_rsa")}" # Path to your SSH key
+  # Triton account information. Example:
+  #
+  # $ make env | grep ^SDC_ACCOUNT | cut -d '=' -f2
+  account      = "my-triton-account-name"
 
-  # Key ID from `triton keys` that matches the file used in key_material
+  # Path to your SSH key
+  key_material = "${file("~/.ssh/id_rsa")}"
+
+  # Key ID from `triton keys` that matches the file used in key_material. Example:
+  #
+  # $ make env | grep ^SDC_KEY_ID | cut -d '=' -f2
   key_id = "de:ad:be:ef:c0:ff:ee:de:ad:be:ef:c0:ff:ee:be:ef"
 
-  # URL from `triton datacenters`
+  # URL from `triton datacenters`.  Example:
+  #
+  # $ make env | grep ^SDC_URL | cut -d '=' -f2
   url = "https://us-west-1.api.joyentcloud.com"
 }
